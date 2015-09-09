4fa241bf8bf94aabe646b27259c50ab351324c10
Koushal Sadaful <ksadaful@live.com>
2015-09-09 23:43:47 +0800

    ecg hk

---
+++ ecghk/javascript/controller/main-controller.js
@@ -0,0 +1,19 @@
+var mainApp = angular.module("mainApp", ['ngRoute']);
+
+mainApp.config(['$routeProvider',
+ function($routeProvider) {
+     debugger;
+    $routeProvider.
+       when('/homepage', {
+          templateUrl: 'html-templates/homepage.html'
+       }).
+       when('/about', {
+          templateUrl: 'html-templates/about.html'
+       }).
+       when('/pagenotfound', {
+                 templateUrl: 'html-templates/page-not-found.html'
+              }).
+       otherwise({
+          redirectTo: '/homepage'
+       });
+ }]);
